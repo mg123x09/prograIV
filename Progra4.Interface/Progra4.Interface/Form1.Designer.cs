f9b617321067924154c2ec92a21f77b46db0a38a
Laboratorios <Laboratorios@E09>
2018-10-27 10:15:54 -0600

    FORM

---
+++ Progra4.Interface/Progra4.Interface/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Progra4.Interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtM1 = new System.Windows.Forms.TextBox();
+            this.txtM2 = new System.Windows.Forms.TextBox();
+            this.txtS1 = new System.Windows.Forms.TextBox();
+            this.txtS2 = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(229, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mensaje 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(232, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mensaje 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(235, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "salida 1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(238, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "salida 2";
+            // 
+            // txtM1
+            // 
+            this.txtM1.Location = new System.Drawing.Point(292, 45);
+            this.txtM1.Name = "txtM1";
+            this.txtM1.Size = new System.Drawing.Size(100, 20);
+            this.txtM1.TabIndex = 4;
+            // 
+            // txtM2
+            // 
+            this.txtM2.Location = new System.Drawing.Point(295, 83);
+            this.txtM2.Name = "txtM2";
+            this.txtM2.Size = new System.Drawing.Size(100, 20);
+            this.txtM2.TabIndex = 5;
+            // 
+            // txtS1
+            // 
+            this.txtS1.Location = new System.Drawing.Point(295, 127);
+            this.txtS1.Name = "txtS1";
+            this.txtS1.Size = new System.Drawing.Size(100, 20);
+            this.txtS1.TabIndex = 6;
+            // 
+            // txtS2
+            // 
+            this.txtS2.Location = new System.Drawing.Point(295, 161);
+            this.txtS2.Name = "txtS2";
+            this.txtS2.Size = new System.Drawing.Size(100, 20);
+            this.txtS2.TabIndex = 7;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(251, 220);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 8;
+            this.btnOK.Text = "ok";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 318);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtS2);
+            this.Controls.Add(this.txtS1);
+            this.Controls.Add(this.txtM2);
+            this.Controls.Add(this.txtM1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtM1;
+        private System.Windows.Forms.TextBox txtM2;
+        private System.Windows.Forms.TextBox txtS1;
+        private System.Windows.Forms.TextBox txtS2;
+        private System.Windows.Forms.Button btnOK;
+    }
+}
+
