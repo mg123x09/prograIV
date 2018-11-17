c677c2869c30d69e69842a9db5341a5c93684aaf
Laboratorios <Laboratorios@E09>
2018-11-17 10:39:32 -0600

    Cambios

---
+++ Progra.IV.AW.Products/Progra.IV.AW.Products.Class/MisTablas/Products.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Progra.IV.AW.Products.Class
+{
+    public partial class Product
+    {
+        public string sizeInInches { get {
+                string elResutado = Size;
+
+                if (SizeUnitMeasureCode != null && SizeUnitMeasureCode == "CM")
+                {
+                    float eltamanoFloat = 0;
+                   if( float.TryParse(Size, out eltamanoFloat))   
+                    elResutado = (eltamanoFloat / (float)2.54).ToString();
+                    
+                }
+                return elResutado;
+                
+            }
+        set { } //* no hay que poner valor por lo que no tinene set
+
+        }
+    }
+}
