796de27c5f3c3b7b058a30511d215625d1f626c4
Laboratorios <Laboratorios@E09>
2018-12-08 10:35:31 -0600

    Practica

---
+++ ModelProyecto/Model/MisTablas/Tiendas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public partial class Tiendas
+    {
+
+        public string Direccion
+        {
+
+            get
+            {
+
+                string result = Provincia + "-" + Canton + "-" + Distrito + ".";
+                if (Distrito == null)
+                {
+                    result = Provincia + "-" + Canton + ".";
+                }
+                return result;
+
+            }
+            set {
+            }
+        }
+    }
+}
+
---
+++ ModelProyecto/Model/MisTablas/Zapatos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public partial class Zapatos
+    {
+        public double? PrecioBruto {
+            get
+            {
+                double? resultado = 0.0;
+                if (Estilo.Contains("niño") || Estilo.Contains("Niña") || Estilo.Contains("girl") || Estilo.Contains("boy") || Estilo.Contains("Child") )
+                {
+                    resultado = PrecioUnitario * 0.20;
+                    resultado = PrecioUnitario - resultado;
+                }
+                if (Estilo.Contains("Dama") || Estilo.Contains("Mujer") || Estilo.Contains("Woman"))
+                {
+                    resultado = PrecioUnitario * 0.10;
+                    resultado = PrecioUnitario + resultado;
+                }
+                if (Estilo.Contains("Caballero") || Estilo.Contains("Hombre") || Estilo.Contains("man"))
+                {
+                    resultado = PrecioUnitario * 0.19;
+                    resultado = PrecioUnitario - resultado;
+                }
+               if (Extento != true)
+                {
+                    resultado = (resultado*0.13)+ resultado;
+                }
+               
+
+                return resultado;
+            }
+
+
+
+
+
+            set { }
+
+        }
+
+        
+}
+}
